3af0ed99bbd06a20a698e896025f76669b4f0a30
yukty2000 <yukty2000@gmail.com>
2022-01-02 13:10:22 +0530

    Created django project

---
+++ Placement_Cell_Website/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Placement_Cell_Website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Placement_Cell_Website.settings')
+
+application = get_wsgi_application()
